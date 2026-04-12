80337aa9ee8262d3066d581a7c9743dc79c46ce1
Michel <audelmichel241@hotmail.com>
2026-04-12 11:35:02 +0200

    ajout todo liste JS fonctinnelle

--- exercice-01/script.js
+++ exercice-01/script.js
@@ -12,3 +12,24 @@ function afficherNom() {
     document.getElementById("resultat").innerText = "Bonjour " + nom;
   }
 }
+
+function ajouterTache() {
+  let tacheInput = document.getElementById("tacheinput");
+  let tache = tacheInput.value;
+
+  if (tache === "") {
+    alert("⚠️ Entre une tâche");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = tache;
+
+  li.onclick = function () {
+    li.remove();
+  };
+
+  document.getElementById("tachelist").appendChild(li);
+
+  tacheInput.value = "";
+}
